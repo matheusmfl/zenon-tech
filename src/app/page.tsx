d14bfc718615320cd4aa8587264a35c0cf6ed51c
Marcelo Barbosa <marcelo.barbosa@ciahering.com.br>
2023-12-07 17:34:57 -0300

    Creating of aboutUs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import AboutUs from '@/components/AboutUs'
 import Section1 from '@/components/Section1'
 
 export default function Home() {
   return (
     <main>
       <Section1 />
+      <AboutUs />
     </main>
   )
 }
