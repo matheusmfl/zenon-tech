59ba50f9fbcc0054593116065e1171212858d60d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-12-07 15:56:52 -0300

    feat: add container component

---
+++ src/components/Section1.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './Container'
+
+function Section1() {
+  return (
+    <section className="bg-slate-950 ">
+      <Container>
+        <h1>Olá</h1>
+      </Container>
+    </section>
+  )
+}
+
+export default Section1
