0fefd0eb63a72ba0674edd03421baf181a5f23c2
Marcelo Barbosa <marcelo.barbosa@ciahering.com.br>
2023-12-08 10:42:17 -0300

    Refactoring spacing and adding divisor

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -5,20 +5,20 @@ import bulletFirst from '../../public/bulletOne.svg'
 import bulletSecond from '../../public/bulletTwo.svg'
 import bulletThird from '../../public/bulletThird.svg'
 import Image from 'next/image'
+import { Separator } from './UI/separator'
 
 function AboutUs() {
   return (
-    <section className="bg-neutral-100">
+    <section className="bg-neutral-100 font-raleWay">
       <Container>
         {/* container */}
         <div className="px-4 py-8">
           <div>
-            {/* ALTERAR ESPAÇAMENTO DEPOIS DO DIVISOR ENTRAR */}
-            <h2 className="text-2xl leading-8 mb-10">
+            <h2 className="text-2xl leading-8 mb-3">
               <b>Somos mais</b> que uma agência de sites
               <span className="text-teal-500">.</span>
             </h2>
-            {/* divisor */}
+            <Separator className="w-[120px] bg-teal-600 h-[6px] mb-[28px]" />
             <ul className="mb-6">
               <li className="flex items-center gap-[10px] mb-[10px]">
                 <span>
@@ -51,9 +51,9 @@ function AboutUs() {
               </li>
             </ul>
           </div>
-          <div>
+          <div className="mt-20">
             <Image
-              className="-ml-[180px]"
+              className="-ml-[165px] scale-[1.4]"
               src={AboutUsMockUp}
               alt="Tela de computador e tela de celular"
             />
